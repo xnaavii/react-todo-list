27c5dfcde5236e937a898696c29442c8f693ab8b
Ivan Petrović <petrovic.valpovo@gmail.com>
2025-11-15 21:12:15 +0000

    feat: render logo and title in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import logoImg from './assets/logo.svg';
+
 function App() {
   return (
     <>
+      <div>
+        <img src={logoImg} alt='logo'/>
+      </div>
       <h1>React todo list</h1>
     </>
   );
