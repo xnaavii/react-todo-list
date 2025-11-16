ffd279ca85513f4a90a788d1b528ceb6142b15d2
Ivan Petrović <petrovic.valpovo@gmail.com>
2025-11-16 10:07:55 +0000

    adjust syling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import TaskInput from './components/TaskInput/TaskInput';
 import Logo from './components/Logo/Logo';
+import classes from './App.module.css';
 
 function App() {
   return (
-    <>
+    <div className={classes.main}>
       <Logo />
       <TaskInput />
-    </>
+    </div>
   );
 }
 
