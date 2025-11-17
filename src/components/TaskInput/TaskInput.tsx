94f3aa1f593aa35c5b26dcda3b7b42829f7d6e4e
Ivan Petrović <petrovic.valpovo@gmail.com>
2025-11-17 15:48:01 +0000

    feat: show an error message upon submitting an empty field

--- src/components/TaskInput/TaskInput.tsx
+++ src/components/TaskInput/TaskInput.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import classes from './TaskInput.module.css';
 
 type TaskInputProps = {
@@ -6,18 +7,31 @@ type TaskInputProps = {
 };
 
 export default function TaskInput({ value, onChange }: TaskInputProps) {
+  const [error, setError] = useState<string>();
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    setError('');
+    if (value === '') {
+      setError('Task name cannot be empty');
+    }
+  }
+
   return (
-    <form className={classes.form}>
-      <input
-        aria-label="Add a task"
-        placeholder="Add a task"
-        className={classes.input}
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-      />
-      <button type="submit" hidden>
-        Add
-      </button>
-    </form>
+    <>
+      <form className={classes.form} onSubmit={handleSubmit}>
+        <input
+          aria-label="Add a task"
+          placeholder="Add a task"
+          className={classes.input}
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+        />
+        <button type="submit" hidden>
+          Add
+        </button>
+      </form>
+      {error && <p>{error}</p>}
+    </>
   );
 }
