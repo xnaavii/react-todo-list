c3854231d7a2f0f9db07ae039960ab0d016ce4f2
Ivan Petrović <petrovic.valpovo@gmail.com>
2025-11-18 20:38:49 +0000

    update styling on the toggle done button

--- src/components/Tasks/TaskItem/TaskItem.tsx
+++ src/components/Tasks/TaskItem/TaskItem.tsx
@@ -1,5 +1,6 @@
 import type { Task } from '../../../types/Task';
 import classes from './TaskItem.module.css';
+import { IoCheckmarkCircle, IoEllipseOutline } from 'react-icons/io5';
 
 type TaskItemProps = {
   task: Task;
@@ -16,12 +17,28 @@ export default function TaskItem({ task, onToggle }: TaskItemProps) {
       <span className={classes.taskName}>{task.name}</span>
 
       <button
-        className={`${classes.button} ${
-          task.completed ? classes.isDone : classes.isPending
-        }`}
+        className={classes.button}
         aria-label={task.completed ? 'Mark as incomplete' : 'Mark as complete'}
         onClick={() => onToggle(task.id)}
-      />
+      >
+        {task.completed ? (
+          // Tasks Done Icon
+          <IoCheckmarkCircle
+            size={24}
+            color={'#448588'}
+            stroke={'#448588'}
+            strokeWidth={1.72}
+          />
+        ) : (
+          // Tasks In Progress Icon
+          <IoEllipseOutline
+            size={24}
+            color={'#5BC0EB'}
+            stroke={'#5BC0EB'}
+            strokeWidth={1.72}
+          />
+        )}
+      </button>
     </li>
   );
 }
