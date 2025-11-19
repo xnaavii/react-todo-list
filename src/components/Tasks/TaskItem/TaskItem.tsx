769e08b2d6b9338faa9a2406f2a09142e250a723
Ivan Petrović <petrovic.valpovo@gmail.com>
2025-11-19 18:32:37 +0000

    feat: add a remove button on a TaskItem

--- src/components/Tasks/TaskItem/TaskItem.tsx
+++ src/components/Tasks/TaskItem/TaskItem.tsx
@@ -1,6 +1,10 @@
 import type { Task } from '../../../types/Task';
 import classes from './TaskItem.module.css';
-import { IoCheckmarkCircle, IoEllipseOutline } from 'react-icons/io5';
+import {
+  IoCheckmarkCircle,
+  IoEllipseOutline,
+  IoTrashOutline,
+} from 'react-icons/io5';
 
 type TaskItemProps = {
   task: Task;
@@ -15,30 +19,36 @@ export default function TaskItem({ task, onToggle }: TaskItemProps) {
       }`}
     >
       <span className={classes.taskName}>{task.name}</span>
-
-      <button
-        className={classes.button}
-        aria-label={task.completed ? 'Mark as incomplete' : 'Mark as complete'}
-        onClick={() => onToggle(task.id)}
-      >
-        {task.completed ? (
-          // Tasks Done Icon
-          <IoCheckmarkCircle
-            size={24}
-            color={'#448588'}
-            stroke={'#448588'}
-            strokeWidth={1.72}
-          />
-        ) : (
-          // Tasks In Progress Icon
-          <IoEllipseOutline
-            size={24}
-            color={'#5BC0EB'}
-            stroke={'#5BC0EB'}
-            strokeWidth={1.72}
-          />
-        )}
-      </button>
+      <menu className={classes.buttons}>
+        <button aria-label="Delete task" className={classes.button}>
+          <IoTrashOutline size={24} />
+        </button>
+        <button
+          className={classes.button}
+          aria-label={
+            task.completed ? 'Mark as incomplete' : 'Mark as complete'
+          }
+          onClick={() => onToggle(task.id)}
+        >
+          {task.completed ? (
+            // Tasks Done Icon
+            <IoCheckmarkCircle
+              size={24}
+              color={'#448588'}
+              stroke={'#448588'}
+              strokeWidth={1.72}
+            />
+          ) : (
+            // Tasks In Progress Icon
+            <IoEllipseOutline
+              size={24}
+              color={'#5BC0EB'}
+              stroke={'#5BC0EB'}
+              strokeWidth={1.72}
+            />
+          )}
+        </button>
+      </menu>
     </li>
   );
 }
